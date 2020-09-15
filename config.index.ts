d080251d8cdc5ffb7512e1d215a90d022b627b62
marianeaudo <marianeaudo@gmail.com>
2020-09-15 12:16:38 +0200

    script config

---
+++ config.index.ts
@@ -0,0 +1,19 @@
+import {writeFile} from 'fs';
+
+const targetPath = './src/environments/environment.prod.ts';
+
+const envConfigFile = `export const environment = {
+   production: true,
+   firebase: {
+        apiKey: '${process.env.FIREBASE_API_KEY}'
+    },
+    name: 'personal-website',
+    version: '0.0.0'
+};
+`;
+
+writeFile(targetPath, envConfigFile, 'utf8', (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
