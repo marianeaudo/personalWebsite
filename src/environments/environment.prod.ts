26b50166a3a1754cef9f9a648a6c8df32adb1404
marianeaudo <marianeaudo@gmail.com>
2020-09-15 16:49:47 +0200

    update environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,16 @@
 import { firebase } from './firebase.environment';
 
 export const environment = {
-   production: true,
-   ...firebase,
-    name: 'personal-website',
-    version: '0.0.0'
+  production: true,
+  firebase: {
+    apiKey: firebase.apiKey,
+    authDomain: firebase.authDomain,
+    databaseURL: firebase.databaseURL,
+    projectId: firebase.projectId,
+    storageBucket: firebase.storageBucket,
+    messagingSenderId: firebase.messagingSenderId,
+    appId: firebase.appId,
+  },
+  name: 'personal-website',
+  version: '0.0.0',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,15 @@ import { firebase } from './firebase.environment';
 
 export const environment = {
     production: true,
-    ...firebase,
+    firebase : {
+      apiKey: firebase.apiKey,
+      authDomain: firebase.authDomain,
+      databaseURL: firebase.databaseURL,
+      projectId: firebase.projectId,
+      storageBucket: firebase.storageBucket,
+      messagingSenderId: firebase.messagingSenderId,
+      appId: firebase.appId
+    },
     name: 'personal-website',
     version: '0.0.0'
 };
