b93a088dd8379b7dbb5ba404103d7c28a2777fc3
marianeaudo <marianeaudo@gmail.com>
2020-09-15 14:28:24 +0200

    update config.index.ts

--- config.index.ts
+++ config.index.ts
@@ -6,7 +6,7 @@ const targetPath2 = './src/environments/environment.prod.ts';
 const envConfigFile = `export const environment = {
    production: true,
    firebase: {
-        apiKey: '${process.env.FIREBASE_API_KEY}'
+        apiKey: '${process.env.FIREBASE_API_KEY}',
         authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
         databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
         projectId: '${process.env.FIREBASE_PROJECT_ID}',
