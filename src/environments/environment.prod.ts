03feaa48a6367fc5a3598b62e91d6e910c95dd4d
marianeaudo <marianeaudo@gmail.com>
2020-09-15 12:21:31 +0200

    environment variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: 'undefinded',
+    authDomain: 'undefinded',
+    databaseURL: 'undefinded',
+    projectId: 'undefinded',
+    storageBucket: 'undefinded',
+    messagingSenderId: 'undefinded',
+    appId: 'undefinded'
+  }
+};
