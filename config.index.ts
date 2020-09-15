7923cdc2163669c1ba6cd58f362abb11aea101fb
marianeaudo <marianeaudo@gmail.com>
2020-09-15 17:13:13 +0200

    update config.index.ts

--- config.index.ts
+++ config.index.ts
@@ -6,8 +6,8 @@ const targetPath2 = './src/environments/environment.ts';
 const envConfigFile = `export const environment = {
    production: true,
    firebase: {
-        apiKey: 'AIzaSyBVrxGibsmzorZ8_9M9CAGDnOBKn-JZXB0',
-        authDomain: 'marianeaudo-website.firebaseapp.com',
+        apiKey: '${process.env.FIREBASE_API_KEY},
+        authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
         databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
         projectId: '${process.env.FIREBASE_PROJECT_ID}',
         storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
