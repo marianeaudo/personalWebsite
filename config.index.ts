fa8f46a4de30124facf4bc5c82dcae617d284b22
marianeaudo <marianeaudo@gmail.com>
2020-09-17 15:52:13 +0200

    use dotenv

--- config.index.ts
+++ config.index.ts
@@ -1,21 +1,40 @@
 import { writeFile } from 'fs';
 
+// read environment variables from .env file
+require('dotenv').config();
+
 const targetPath = './src/environments/environment.prod.ts';
 const targetPath2 = './src/environments/environment.ts';
 
 const envConfigFile = `export const environment = {
    production: true,
    firebase: {
-        apiKey: '${process.env.FIREBASE_API_KEY}',
-        authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
-        databaseURL: '${process.env.FIREBASE_DATABASE_URL}',
-        projectId: '${process.env.FIREBASE_PROJECT_ID}',
-        storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
-        messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}',
-        appId: '${process.env.FIREBASE_APP_ID}'
+        apiKey: '${process.env.API_KEY}',
+        authDomain: '${process.env.FAUTH_DOMAIN}',
+        databaseURL: '${process.env.DATABASE_URL}',
+        projectId: '${process.env.PROJECT_ID}',
+        storageBucket: '${process.env.STORAGE_BUCKET}',
+        messagingSenderId: '${process.env.MESSAGING_SENDER_ID}',
+        appId: '${process.env.APP_ID}'
     },
     name: 'personal-website',
     version: '0.0.0'
+};
+`;
+
+const envConfigFile2 = `export const environment = {
+  production: false,
+  firebase: {
+       apiKey: '${process.env.API_KEY}',
+       authDomain: '${process.env.FAUTH_DOMAIN}',
+       databaseURL: '${process.env.DATABASE_URL}',
+       projectId: '${process.env.PROJECT_ID}',
+       storageBucket: '${process.env.STORAGE_BUCKET}',
+       messagingSenderId: '${process.env.MESSAGING_SENDER_ID}',
+       appId: '${process.env.APP_ID}'
+   },
+   name: 'personal-website',
+   version: '0.0.0'
 };
 `;
 
@@ -25,7 +44,7 @@ writeFile(targetPath, envConfigFile, 'utf8', (err) => {
   }
 });
 
-writeFile(targetPath2, envConfigFile, 'utf8', (err) => {
+writeFile(targetPath2, envConfigFile2, 'utf8', (err) => {
   if (err) {
     return console.log(err);
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,13 @@
-import { firebase } from './firebase.environment';
-
 export const environment = {
    production: true,
    firebase: {
-    apiKey: firebase.apiKey,
-    authDomain: firebase.authDomain,
-    databaseURL: firebase.databaseURL,
-    projectId: firebase.projectId,
-    storageBucket: firebase.storageBucket,
-    messagingSenderId: firebase.messagingSenderId,
-    appId: firebase.appId
+        apiKey: 'AIzaSyBVrxGibsmzorZ8_9M9CAGDnOBKn-JZXB0',
+        authDomain: 'undefined',
+        databaseURL: 'https://marianeaudo-website.firebaseio.com',
+        projectId: 'marianeaudo-website',
+        storageBucket: 'marianeaudo-website.appspot.com',
+        messagingSenderId: '444882523452',
+        appId: '1:444882523452:web:1e453ef0d6bd63d54d3a39'
     },
     name: 'personal-website',
     version: '0.0.0'
