552fa7124e6d1736d0b2f3e180da45ef794eb36e
marianeaudo <marianeaudo@gmail.com>
2020-09-17 14:40:14 +0200

    update environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,15 @@
+import { firebase } from './firebase.environment';
+
 export const environment = {
    production: true,
    firebase: {
-        apiKey: 'undefined',
-        authDomain: 'undefined',
-        databaseURL: 'undefined',
-        projectId: 'undefined',
-        storageBucket: 'undefined',
-        messagingSenderId: 'undefined',
-        appId: 'undefined'
+    apiKey: firebase.apiKey,
+    authDomain: firebase.authDomain,
+    databaseURL: firebase.databaseURL,
+    projectId: firebase.projectId,
+    storageBucket: firebase.storageBucket,
+    messagingSenderId: firebase.messagingSenderId,
+    appId: firebase.appId
     },
     name: 'personal-website',
     version: '0.0.0'
