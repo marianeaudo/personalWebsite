91c615287ca2c9324bd3d2d255bc2ea1e48512ef
marianeaudo <marianeaudo@gmail.com>
2020-09-17 14:17:58 +0200

    update config.index.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,13 @@
-import { firebase } from './firebase.environment';
-
 export const environment = {
-    production: true,
-    firebase : {
-      apiKey: firebase.apiKey,
-      authDomain: firebase.authDomain,
-      databaseURL: firebase.databaseURL,
-      projectId: firebase.projectId,
-      storageBucket: firebase.storageBucket,
-      messagingSenderId: firebase.messagingSenderId,
-      appId: firebase.appId
+   production: true,
+   firebase: {
+        apiKey: 'undefined',
+        authDomain: 'undefined',
+        databaseURL: 'undefined',
+        projectId: 'undefined',
+        storageBucket: 'undefined',
+        messagingSenderId: 'undefined',
+        appId: 'undefined'
     },
     name: 'personal-website',
     version: '0.0.0'
