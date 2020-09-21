33a08c6aecadb36ffa0336fcfefdb67da5336e3a
marianeaudo <marianeaudo@gmail.com>
2020-09-21 20:16:27 +0200

    config.index.ts update

--- config.index.ts
+++ config.index.ts
@@ -1,10 +1,12 @@
 import { writeFile } from 'fs';
+import { environment } from './src/environments/environment';
+
 
 // read environment variables from .env file
 require('dotenv').config();
 
 const targetPath = './src/environments/environment.prod.ts';
-const targetPath2 = './src/environments/environment.ts';
+// const targetPath2 = './src/environments/environment.ts';
 
 const envConfigFile = `export const environment = {
    production: true,
@@ -22,30 +24,35 @@ const envConfigFile = `export const environment = {
 };
 `;
 
-const envConfigFile2 = `export const environment = {
-  production: false,
-  firebase: {
-       apiKey: '${process.env.API_KEY}',
-       authDomain: '${process.env.AUTH_DOMAIN}',
-       databaseURL: '${process.env.DATABASE_URL}',
-       projectId: '${process.env.PROJECT_ID}',
-       storageBucket: '${process.env.STORAGE_BUCKET}',
-       messagingSenderId: '${process.env.MESSAGING_SENDER_ID}',
-       appId: '${process.env.APP_ID}'
-   },
-   name: 'personal-website',
-   version: '0.0.0'
-};
-`;
+// const envConfigFile2 = `export const environment = {
+//   production: false,
+//   firebase: {
+//        apiKey: '${process.env.API_KEY}',
+//        authDomain: '${process.env.AUTH_DOMAIN}',
+//        databaseURL: '${process.env.DATABASE_URL}',
+//        projectId: '${process.env.PROJECT_ID}',
+//        storageBucket: '${process.env.STORAGE_BUCKET}',
+//        messagingSenderId: '${process.env.MESSAGING_SENDER_ID}',
+//        appId: '${process.env.APP_ID}'
+//    },
+//    name: 'personal-website',
+//    version: '0.0.0'
+// };
+// `;
 
-writeFile(targetPath, envConfigFile, 'utf8', (err) => {
-  if (err) {
-    return console.log(err);
-  }
-});
+if (environment.production) {
+  writeFile(targetPath, envConfigFile, 'utf8', (err) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
+}
 
-writeFile(targetPath2, envConfigFile2, 'utf8', (err) => {
-  if (err) {
-    return console.log(err);
-  }
-});
+
+
+
+// writeFile(targetPath2, envConfigFile2, 'utf8', (err) => {
+//   if (err) {
+//     return console.log(err);
+//   }
+// });
