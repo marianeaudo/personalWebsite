c68b2b90e813df37bc94166317afe594019f1438
marianeaudo <marianeaudo@gmail.com>
2020-09-23 14:41:22 +0200

    update environment file

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebase: {
+   apiKey: undefined,
+   authDomain: undefined,
+   databaseURL: undefined,
+   projectId: undefined,
+   storageBucket: undefined,
+   messagingSenderId: undefined,
+   appId: undefined
+   },
+   name: 'personal-website',
+   version: '0.0.0'
+};
