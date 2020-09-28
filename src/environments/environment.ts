f84d99c728b78ec4e18f1b2bbc6d85f371ac168e
marianeaudo <marianeaudo@gmail.com>
2020-09-28 13:17:17 +0200

    about.html modification

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-   apiKey: undefined,
-   authDomain: undefined,
-   databaseURL: undefined,
-   projectId: undefined,
-   storageBucket: undefined,
-   messagingSenderId: undefined,
-   appId: undefined
+    apiKey: undefined,
+    authDomain: undefined,
+    databaseURL: undefined,
+    projectId: undefined,
+    storageBucket: undefined,
+    messagingSenderId: undefined,
+    appId: undefined
    },
    name: 'personal-website',
    version: '0.0.0'
